dbbe55f360d3acb91cb34f83d66912274276c6a7
Joe <joe45moses@gmail.com>
2020-11-28 06:47:43 +0000

    Refactored errors to use an array

--- index.php
+++ index.php
@@ -1,78 +1,80 @@
 <?php
-$error = "";
+$error = [];
+$error_list_imploded = '';
 $successMessage = "";
 if ($_POST) {
 
-  if (!isset($_POST["email"]) || empty($_POST["email"])) {
+    if (!isset($_POST["email"]) || empty($_POST["email"])) {
 
-    $error .= "An email address is required.<br>";
+        $error[] = "An email address is required";
 
-  }
+    }
 
-  if (!isset($_POST["message"]) || empty($_POST["message"])) {
-    $error .= "The message field is required.<br>";
+    if (!isset($_POST["message"]) || empty($_POST["message"])) {
+        $error[] = "The message field is required.";
 
-  }
+    }
 
-  if (!isset($_POST["subject"]) || empty($_POST["subject"])) {
-    $error .= "The subject is required.<br>";
+    if (!isset($_POST["subject"]) || empty($_POST["subject"])) {
+        $error[] = "The subject is required";
 
-  }
+    }
 
 
-  if (!isset($_POST["email"]) ||  filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) === false) {
+    if (!isset($_POST["email"]) ||  filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) === false) {
 
-    $error .= "The email address is invalid.<br>";
+        $error[] = "The email address is invalid.";
 
-  }
-  if ($error != "") {
-    $error = '<div class="alert alert-danger" role="alert"><p><strong>There were error(s) in your form:</strong></p>' . $error . '</div>';
-  } else {
-  	$emailTo = "jeffmchaledesigns@gmail.com";
-  	$subject = $_POST['subject'];
-  	$message = $_POST['message'];
-  	$headers = "From: ".$_POST['email'];
-  	if(mail($emailTo, $subject, $message, $headers)) {
-  		$successMessage = '<div class="alert alert-success" role="alert"><strong>Your message was sent. We\'ll get back to you ASAP!</strong></div>';
-	  } else {
-  		$error = '<div class="alert alert-danger" role="alert"><strong>Your message could not be sent.</strong>' . $error . '</div>';
-	  }
-  }
+    }
+    if (count($error)) {
+        $error_list_imploded = implode('<br>', $error);
+        $error_markup = '<div class="alert alert-danger" role="alert"><p><strong>There were error(s) in your form:</strong></p>' . $error_list_imploded . '</div>';
+    } else {
+        $emailTo = "jeffmchaledesigns@gmail.com";
+        $subject = $_POST['subject'];
+        $message = $_POST['message'];
+        $headers = "From: ".$_POST['email'];
+        if(mail($emailTo, $subject, $message, $headers)) {
+            $successMessage = '<div class="alert alert-success" role="alert"><strong>Your message was sent. We\'ll get back to you ASAP!</strong></div>';
+        } else {
+            $error_markup = '<div class="alert alert-danger" role="alert"><strong>Your message could not be sent.</strong>' . $error_list_imploded . '</div>';
+        }
+    }
 }
 ?>
 <!doctype html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport"
-	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Contact Form</title>
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-	      integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact Form</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+          integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 <body>
 <div class="container d-flex flex-column">
-	<h1>Get in touch!</h1>
-	<div id="error"><?= $error ?></div>
-	<div>
-		<form method="post">
-			<div class="form-group">
-				<label for="email">Enter you e-mail address</label>
-				<input type="email" name="email" class="form-control" id="email" placeholder="example@email.com">
-				<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone.</small>
-			</div>
-			<div class="form-group">
-				<label for="subject">Subject</label>
-				<input type="text" name="subject" class="form-control" id="subject">
-			</div>
-			<div class="form-group">
-				<label for="message">What would you like to ask?</label>
-				<textarea name="message" id="message" rows="4" class="form-control"></textarea>
-			</div>
-			<button type="submit" class="btn btn-primary" id="submit">Send</button>
-		</form>
-	</div>
+    <h1>Get in touch!</h1>
+    <div id="error"><?= $error_markup ?></div>
+    <div>
+        <form method="post">
+            <div class="form-group">
+                <label for="email">Enter you e-mail address</label>
+                <input type="email" name="email" class="form-control" id="email" placeholder="example@email.com">
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone.</small>
+            </div>
+            <div class="form-group">
+                <label for="subject">Subject</label>
+                <input type="text" name="subject" class="form-control" id="subject">
+            </div>
+            <div class="form-group">
+                <label for="message">What would you like to ask?</label>
+                <textarea name="message" id="message" rows="4" class="form-control"></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary" id="submit">Send</button>
+        </form>
+    </div>
 </div>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
